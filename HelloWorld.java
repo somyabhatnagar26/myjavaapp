70a168fbaad409b158e7219ca66510acca710b82
Somya Bhatnagar <bhtngr.somya@gmail.com>
2020-12-11 23:10:05 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("Hello world from myjavaapp");
+        System.out.println("Another line added");
+        int num =10 ;
+    }
+}
